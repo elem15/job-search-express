b006e4e12c71aefa31674502d1434b78944e0166
Elem <elem15@yandex.ru>
2023-06-30 14:55:17 +0300

    fix: send empty arr instead message if nothing found

--- src/controllers/get-developers.js
+++ src/controllers/get-developers.js
@@ -5,29 +5,29 @@ import { fileURLToPath } from 'node:url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const src = path.join(__dirname, '..', 'data', 'db.json');
 
-const getJobsByTitle = async () => {
+const getJobs = async () => {
   try {
     const json = await readFile(src, 'utf-8');
     const data = JSON.parse(json);
     return data;
   } catch (error) {
     console.log(error);
-    return { message: "Something went wrong" };
+    return [];
   }
 };
 
 export async function getReactDevelopers(query) {
-  const data = await getJobsByTitle();
-  if (query) {
+  const data = await getJobs();
+  if (query && data.length) {
     const result = data.filter(job => job.job_title.toLowerCase().includes(query.toLowerCase()));
     if (result.length) return result;
-    return { message: "Nothing found" };
+    return [];
   }
   return data;
 }
 
 export async function getDevelopersById(id) {
-  const data = await getJobsByTitle();
+  const data = await getJobs();
   const result = data.find(job => job.id == id);
   if (result) return result;
   return { message: "Nothing found" };
